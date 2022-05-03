3d95e15b243e69474ff5a061bcb19c3bc1f2d890
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-05-03 22:36:12 +0600

    get specific inventory

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.port || 5000;
 
@@ -12,14 +14,49 @@ app.use(cors());
 // DB_USER=VegetablesPlanet
 // DB_PASS=mJnZRjGeebGXU63n
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://<username>:<password>@cluster0.wmrfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wmrfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async() => {
+    try{
+        await client.connect();
+        const vegetablesCollection = client.db("VegetablesPlanet").collection("Vegetables");
+
+        // Get Inventories
+        app.get('/inventories', async(req, res) => {
+            const query = {};
+            const cursor = vegetablesCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // get Inventory
+        app.get('/inventory/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const inventory = await vegetablesCollection.findOne(query);
+            res.send(inventory);
+        })
+
+        // get delivery
+        app.get('/inventory/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+
+        })
+
+        // create inventory
+        app.post('/inventory', (req, res) => {
+            
+        })
+
+    }finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("Running Vegetables Planet Server")
