860211def38995a7e41678f5de08de6536e8f8a1
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-05-07 22:44:28 +0600

    update port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
